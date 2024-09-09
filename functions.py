0ec7dce4441aaa8200a26096e6745b7fcb6e4c3c
AlejandroAlegrePinilla <alexalegrepinilla@gmail.com>
2024-09-09 18:02:38 +0200

    FitRing

--- functions.py
+++ functions.py
@@ -31,3 +31,28 @@ def plot_bar_with_values(df, x_column, y_column, title, xlabel, ylabel, color='s
     
     # Mostrar el gráfico
     plt.show()
+
+def plot_bar_without_values(df, x_column, y_column, title, xlabel, ylabel, color='skyblue'):
+    """
+    Función para crear un gráfico de barras con valores sobre las barras.
+    
+    :param df: DataFrame con los datos a graficar
+    :param x_column: Columna del DataFrame para el eje X (categorías)
+    :param y_column: Columna del DataFrame para el eje Y (valores)
+    :param title: Título del gráfico
+    :param xlabel: Etiqueta para el eje X
+    :param ylabel: Etiqueta para el eje Y
+    :param color: Color de las barras (opcional, por defecto 'skyblue')
+    """
+    plt.figure(figsize=(10,6))
+    
+    # Crear el gráfico de barras
+    bars = plt.bar(df[x_column], df[y_column], color=color)
+    
+    # Añadir título y etiquetas
+    plt.title(title, fontsize=16)
+    plt.xlabel(xlabel, fontsize=14)
+    plt.ylabel(ylabel, fontsize=14)
+    
+    # Mostrar el gráfico
+    plt.show()
